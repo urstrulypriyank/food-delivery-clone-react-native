8b3662d519eff58efb51ee24b808e1755f54303f
priyank rai <0priyankrai@gmail.com>
2023-02-07 21:01:50 +0530

    minro

--- Components/ResturantComponent.js
+++ Components/ResturantComponent.js
@@ -2,7 +2,7 @@
 
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
-import { StarIcon } from "react-native-heroicons/solid";
+import { StarIcon, MapPinIcon } from "react-native-heroicons/solid";
 export default function ResturantComponent({
   id,
   imgUrl,
@@ -16,20 +16,19 @@ export default function ResturantComponent({
   latitude,
 }) {
   return (
-    <TouchableOpacity>
-      <Image source={{ uri: imgUrl }} className="h-36 w-64" />
+    <TouchableOpacity className="bg-white mr-3">
+      <Image source={{ uri: imgUrl }} className="h-36 w-64 rounded-sm" />
       <View className="px-3 pb-3 ">
-        
         <Text className="font-bold text-lg pt-2">{title}</Text>
-        
-        {/* View for rating and genre */}
         <View className="flex-row items-center space-x-1">
           <StarIcon color="green" opacity={0.5} size={20} />
           <Text className="text-xs text-gray-500">
-            <Text className="text-green-500"> 
-            {rating}
-            </Text>
-            · {genre}</Text>
+            <Text className="text-green-500">{rating}</Text>· {genre}
+          </Text>
+        </View>
+        <View className="flex-row space-x-1">
+          <MapPinIcon color="gray" opacity={0.4} size={22} />
+          <Text className="text-xs text-gray-500">Nearby · {address}</Text>
         </View>
       </View>
     </TouchableOpacity>
